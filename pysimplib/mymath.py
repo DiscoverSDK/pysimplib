30de74cdbfbef23481995699f6579dc5ccd86da0
liran <liran@example.com>
2016-12-01 22:59:13 +0200

    basic

---
+++ pysimplib/mymath.py
@@ -0,0 +1,5 @@
+def myadd(a,b):
+    return a+b;
+
+def mysub(a,b):
+    return a-b;
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pysimplib',
+  packages = ['pysimplib'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A tutorial lib',
+  author = 'Liran BH',
+  author_email = 'liran@discoversdk.com',
+  url = 'https://github.com/DiscoverSDK/pysimplib',
+  download_url = 'https://github.com/DiscoverSDK/pysimplib/tarball/0.1',
+  keywords = ['add', 'sub', 'tests'], 
+  classifiers = [],
+)
