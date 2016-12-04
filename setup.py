1bfc65df6e26efe748b797de4ee7d8f7dc7c2b35
liran <liran@example.com>
2016-12-04 23:04:46 +0200

    final2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pysimplib',
   packages = ['pysimplib'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A tutorial lib',
   author = 'Liran BH',
   author_email = 'liran@discoversdk.com',
   url = 'https://github.com/DiscoverSDK/pysimplib',
   download_url = 'https://github.com/DiscoverSDK/pysimplib/tarball/0.1',
-  keywords = ['add', 'sub', 'tests'], 
+  keywords = ['add', 'sub', 'tests'],
   classifiers = [],
 )
